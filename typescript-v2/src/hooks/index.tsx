55124ab94f1c72943c015f60bc11525a427afead
dikshit-n <62920150+dikshit-n@users.noreply.github.com>
2022-07-16 11:37:46 +0530

    useQuery hook

--- typescript-v2/src/hooks/index.tsx
+++ typescript-v2/src/hooks/index.tsx
@@ -5,5 +5,6 @@ export * from "./useOutSideClick";
 export * from "./useUniqueKey";
 export * from "./useAuth";
 // export * from './useOAuth'
+export * from "./useQueryState";
 
 export { useSelector, useDispatch };
